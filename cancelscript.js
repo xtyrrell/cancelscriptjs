e34031859c6f92199ef745bd3733f7b7a46ab474
Max <maxtyrrell0@gmail.com>
2019-01-21 19:09:06 +0200

    Update cancelscript.js

--- cancelscript.js
+++ cancelscript.js
@@ -7,17 +7,17 @@ function cancelScript(source) {
     i++
 
     if (char.trim() === '') {
-      console.log('Character is whitespace')
+      // console.log('Character is whitespace')
 
       continue
     }
     if (source.slice(i, i + 'cancel'.length + 1).toLowerCase() == 'cancel') {
-      console.log('The following word is "cancel". Terminating.')
+      // console.log('The following word is "cancel". Terminating.')
 
       process.exit(0)
     }
     if (char) {
-      console.log(`Character '${char}' is unrecognised non-whitespace input. Terminating.`)
+      // console.log(`Character '${char}' is unrecognised non-whitespace input. Terminating.`)
 
       process.exit(0)
     }
