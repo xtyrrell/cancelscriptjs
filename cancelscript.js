84158761bced195374c3922f71b73af35e4ad91b
Max <maxtyrrell0@gmail.com>
2019-01-21 19:05:42 +0200

    Create cancelscript.js

---
+++ cancelscript.js
@@ -0,0 +1,29 @@
+// A naive implementation of CancelScript in fairly modern JavaScript
+
+function cancelScript(source) {
+  let i = -1
+
+  for (const char of source) {
+    i++
+
+    if (char.trim() === '') {
+      console.log('Character is whitespace')
+
+      continue
+    }
+    if (source.slice(i, i + 'cancel'.length + 1).toLowerCase() == 'cancel') {
+      console.log('The following word is "cancel". Terminating.')
+
+      process.exit(0)
+    }
+    if (char) {
+      console.log(`Character '${char}' is unrecognised non-whitespace input. Terminating.`)
+
+      process.exit(0)
+    }
+
+    i++
+  }
+}
+
+module.exports = cancelScript
